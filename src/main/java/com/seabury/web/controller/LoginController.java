d431376c9475794188906054d74f4713d35542ec
ms.ko <ms.ko@ssap.kr>
2023-06-12 15:33:21 +0900

    Add login Page

---
+++ src/main/java/com/seabury/web/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.seabury.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+    @RequestMapping(value={"/login"}, method = RequestMethod.GET)
+    public ModelAndView example(ModelAndView mav){
+        mav.setViewName("login/login");
+        return mav;
+    }
+}
