77fc46cdf02bdfe0d3e408a281059a050327666a
SSK-yckwon <yc.kwon@seaburysolutions.com>
2023-10-04 15:20:48 +0900

    Update RadiationSource and Entity

--- src/main/java/com/seabury/web/entity/integrated/ND_60_EXT_Entity.java
+++ src/main/java/com/seabury/web/entity/integrated/ND_60_EXT_Entity.java
@@ -6,30 +6,30 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ND_60_EXT_Entity {
+	String nuclide;
 	float adrenals;
 	float bladder_wall;
 	float bone_surface;
 	float brain;
 	float breast;
-	float e;
 	float esophagus;
+	float stomach_wall;
+	float small_intestine_wall;
+	float upper_large_intestine_wall;
+	float lower_large_intestine_wall;
 	float kidneys;
 	float liver;
-	float lower_large_intestine_wall;
 	float lungs;
 	float muscle;
-	String nuclide;
 	float ovaries;
 	float pancreas;
 	float red_marrow;
 	float skin;
-	float small_intestine_wall;
 	float spleen;
-	float stomach_wall;
 	float testes;
 	float thymus;
 	float thyroid;
-	float upper_large_intestine_wall;
 	float uterus;
 	float he;
+	float e;
 }
